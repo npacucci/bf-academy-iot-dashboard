dea4ee2634ef49721d248c26d838b34caeaa3e94
Giuseppe Liso <giuseppe.liso@burda-forward.it>
2024-04-03 11:47:52 +0200

    added exercises

--- src/routes/exercises/+page.svelte
+++ src/routes/exercises/+page.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-   import ComponenteEsempio1 from "$lib/components/exercises/ComponenteEsempio1.svelte";
-   import ComponenteEsempio2 from "$lib/components/exercises/ComponenteEsempio2.svelte";
    import { Modal} from 'flowbite-svelte';
-   import ComponenteEsempio3 from "$lib/components/exercises/ComponenteEsempio3.svelte";
-   import ComponenteEsempio4 from "$lib/components/exercises/ComponenteEsempio4.svelte";
-   import ComponenteEsempio5 from "$lib/components/exercises/ComponenteEsempio5.svelte";
-   import ComponenteEsempio6 from "$lib/components/exercises/ComponenteEsempio6.svelte";
    let currentSelection = 1;
    let items=[1,2,3,4,5,6];
    let modalOpened = false;
@@ -33,19 +27,7 @@
     </ul>
 </div>
 <div class="main-container">
-    {#if currentSelection===1}
-        <ComponenteEsempio1 ></ComponenteEsempio1>
-        {:else if  currentSelection===2}
-            <ComponenteEsempio2 on:message={extractInfo}></ComponenteEsempio2>
-        {:else if currentSelection===3}
-          <ComponenteEsempio3></ComponenteEsempio3>
-    {:else if currentSelection===4}
-        <ComponenteEsempio4></ComponenteEsempio4>
-    {:else if currentSelection===5}
-        <ComponenteEsempio5 ></ComponenteEsempio5>
-    {:else if currentSelection===6}
-        <ComponenteEsempio6></ComponenteEsempio6>
-    {/if}
+
 </div>
 
 <Modal title="Modal example" bind:open={modalOpened} on:close={() => modalOpened = false}>
